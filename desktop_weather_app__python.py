02539c3758b31bcbf9682a44221907bd4586bf5b
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-04-18 17:20:13 +0200

    fix datetime object

--- desktop_weather_app__python.py
+++ desktop_weather_app__python.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import requests
 from datetime import datetime
+import pytz
 
 # initialize window
 
@@ -11,15 +12,20 @@ root.title("Weather App")  # title of window
 
 # create functionality to change time format
 
+
 def time_format_for_location(utc_with_tz):
-    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    local_time = datetime.fromtimestamp(utc_with_tz, pytz.utc)
     return local_time.time()
 
 # style window
 
 city_value = tk.StringVar(root, "")
-city_head = tk.Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)  # to generate label heading
-inp_city = tk.Entry(root, textvariable=city_value, width=24, font='Arial 14 bold').pack()  # entry field
+
+
+# to generate label heading
+city_head = tk.Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)
+# entry field
+inp_city = tk.Entry(root, textvariable=city_value, width=24, font='Arial 14 bold').pack()
 
 # create functionality for retrieving weather data from OpenWeather.com
 
@@ -27,40 +33,61 @@ api_key = ""
 with open("API_key.txt") as key:
     api_key = key.read()
 
+
 def show_weather():
-    city_name = city_value.get()  # Get city name from user from the input field (later in the code)
+    # Get city name from user from the input field (later in the code)
+    city_name = city_value.get()
     weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid='+api_key  # API url
     response = requests.get(weather_url)  # Get the response from fetched url
-    weather_info = response.json()  # changing response from json to python readable 
+    weather_info = response.json()  # changing response from json to python readable
     tfield.delete("1.0", "end")  # to clear the text field for every new output
-    
-    # as per API documentation, if the cod is 200, it means that weather data was successfully fetched
+
+    # as per API documentation, if the code is 200, it means that weather data was
+    # successfully fetched
     if weather_info['cod'] == 200:
         kelvin = 273  # value of kelvin
- 
-# -----------Storing the fetched values of weather of a city
- 
-        temp = int(weather_info['main']['temp'] - kelvin)  #converting default kelvin value to Celcius
+
+        # -----------Storing the fetched values of weather of a city
+
+        # converting default kelvin value to Celcius
+        temp = int(weather_info['main']['temp'] - kelvin)
         feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
-        pressure = weather_info['main']['pressure']
         humidity = weather_info['main']['humidity']
-        wind_speed = weather_info['wind']['speed'] * 3.6
         sunrise = weather_info['sys']['sunrise']
         sunset = weather_info['sys']['sunset']
         timezone = weather_info['timezone']
         cloudy = weather_info['clouds']['all']
         description = weather_info['weather'][0]['description']
- 
+
         sunrise_time = time_format_for_location(sunrise + timezone)
         sunset_time = time_format_for_location(sunset + timezone)
- 
+
 # assigning Values to our weather varaible, to display as output
-         
-        weather = f"\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
+
+        weather = f"""
+        Weather of: {city_name}
+        Temperature (Celsius): {temp}°
+        Feels like in (Celsius): {feels_like_temp}°
+        Humidity: {humidity}%
+        Sunrise at {sunrise_time}
+        Sunset at {sunset_time}
+        Cloud: {cloudy}%
+        Info: {description}
+        """
     else:
-        weather = f"\n\tWeather for '{city_name}' not found!\n\tKindly Enter valid City Name !!"
- 
+        weather = f"""
+        Weather for
+
+        {city_name.upper()}
+
+        not found!
+
+
+        Please, check your spelling!
+        """
+
     tfield.insert("1.0", weather)  # to insert or send value in our Text Field to display output
+
 
 tk.Button(root, command=show_weather, text="Check Weather", font="Arial 10", bg='lightblue', fg='black', activebackground="teal", padx=5, pady=5).pack(pady=20)
 
