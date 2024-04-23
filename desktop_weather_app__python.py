5b42ada803b3bb5bed85ed4760c96bf994aea54b
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-04-23 20:58:24 +0200

    refactor, add type annotations and docstrings

--- desktop_weather_app__python.py
+++ desktop_weather_app__python.py
@@ -3,56 +3,35 @@ import requests
 from datetime import datetime
 import pytz
 
-# initialize window
 
-root = tk.Tk()
-root.geometry("400x500")  # size of the window by default
-root.resizable(0, 0)  # to make the window size fixed
-root.title("Weather App")  # title of window
-
-# create functionality to change time format
-
-
-def time_format_for_location(utc_with_tz):
+def time_format_for_location(utc_with_tz: datetime) -> datetime:
+    """Change time format."""
     local_time = datetime.fromtimestamp(utc_with_tz, pytz.utc)
     return local_time.time()
 
-# style window
 
-
-city_value = tk.StringVar(root, "")
-
-
-# to generate label heading
-city_head = tk.Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)
-# entry field
-inp_city = tk.Entry(root, textvariable=city_value, width=24, font='Arial 14 bold').pack()
-
-# create functionality for retrieving weather data from OpenWeather.com
-
-api_key = ""
+api_key = ""  # stores API key for retrieving weather data from OpenWeather.com
 with open("API_key.txt") as key:
     api_key = key.read()
 
 
-def show_weather(city_name=None):
-    # Get city name from user from the input field (later in the code)
+def show_weather(city_name: str = None):
+    """
+    Get city name from user from the input field (later in the code)
+    """
     if not city_name:
         city_name = city_value.get()
     base_url = 'http://api.openweathermap.org/data/2.5/weather?q='
     weather_url = base_url + city_name + '&appid=' + api_key  # API url
-    response = requests.get(weather_url)  # Get the response from fetched url
-    weather_info = response.json()  # changing response from json to python readable
-    tfield.delete("1.0", "end")  # to clear the text field for every new output
+    response = requests.get(weather_url)  # get response from fetched url
+    weather_info = response.json()  # change response from json to python readable
+    tfield.delete("1.0", "end")  # clear text field for every new output
 
-    # as per API documentation, if the code is 200, it means that weather data was
-    # successfully fetched
-    if weather_info['cod'] == 200:
-        kelvin = 273  # value of kelvin
+    if weather_info['cod'] == 200:  # as per API documentation, if the code is 200, it means that
+        # weather data was successfully fetched
+        kelvin = 273
 
-        # -----------Storing the fetched values of weather of a city
-
-        # converting default kelvin value to Celcius
+        # -----------store fetched values
         temp = int(weather_info['main']['temp'] - kelvin)
         feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
         humidity = weather_info['main']['humidity']
@@ -61,12 +40,10 @@ def show_weather(city_name=None):
         timezone = weather_info['timezone']
         cloudy = weather_info['clouds']['all']
         description = weather_info['weather'][0]['description']
-
         sunrise_time = time_format_for_location(sunrise + timezone)
         sunset_time = time_format_for_location(sunset + timezone)
-
-# assigning Values to our weather varaible, to display as output
-
+        
+        # ----------assign values to weather varaible, to be displayed as output
         weather = f"""
         Weather of: {city_name}
         Temperature (Celsius): {temp}°
@@ -89,33 +66,39 @@ def show_weather(city_name=None):
         Please, check your spelling!
         """
 
-    tfield.insert("1.0", weather)  # to insert or send value in our Text Field to display output
+    tfield.insert("1.0", weather)  # send value text field to display output
 
 
-tk.Button(root, command=show_weather, text="Check Weather", font="Arial 10", bg='lightblue',
-          fg='black', activebackground="teal", padx=5, pady=5).pack(pady=20)
+root = tk.Tk()  # initialize window
+root.geometry("400x500")  # size of the window by default
+root.resizable(0, 0)  # to make the window size fixed
+root.title("Weather App")  # title of window
 
-# Displaying weather data
+
+city_value = tk.StringVar(root, "")
+city_head = tk.Label(root, text='Enter City Name', font='Arial 12 bold').pack(pady=10)
+inp_city = tk.Entry(root, textvariable=city_value, width=24, font='Arial 14 bold').pack()
+
+
+generate_weather = tk.Button(root, command=show_weather, text="Check Weather", font="Arial 10",
+                             bg='lightblue', fg='black', activebackground="teal", padx=5,
+                             pady=5).pack(pady=20).bind("<Return>")
 
 weather_now = tk.Label(root, text="The Weather is: ", font='arial 12 bold').pack(pady=10)
-
 tfield = tk.Text(root, width=46, height=10)
 tfield.pack()
 
-fav1 = tk.Button(root, command=lambda: show_weather("Avignon"), text="Avignon", font="Arial 10", 
-                 bg='lightyellow', fg='black', activebackground="teal", padx=5, pady=5)
+fav1 = tk.Button(root, command=lambda: show_weather("Avignon"), text="Avignon", font="Arial 10",
+                 bg='lightyellow', fg='black', activebackground="teal", padx=5, 
+                 pady=5).pack(side="left", expand=True)
 
 
 fav2 = tk.Button(root, command=lambda: show_weather("Kitzbühel"), text="Kitzbühel",
                  font="Arial 10", bg='thistle', fg='black', activebackground="teal",
-                 padx=5, pady=5)
+                 padx=5, pady=5).pack(side="left", expand=True)
 
 fav3 = tk.Button(root, command=lambda: show_weather("Amsterdam"), text="Amsterdam",
-                 font="Arial 10", bg='aquamarine', fg='black', activebackground="teal",
-                 padx=5, pady=5)
-
-fav1.pack(side="left", expand=True)
-fav2.pack(side="left", expand=True)
-fav3.pack(side="left", expand=True)
+                 font="Arial 10", bg='pale green', fg='black', activebackground="teal",
+                 padx=5, pady=5).pack(side="left", expand=True)
 
 root.mainloop()
